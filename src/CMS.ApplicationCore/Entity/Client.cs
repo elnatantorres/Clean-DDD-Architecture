8d77f7da86892a82e5b4309778b7702f1e61db19
Elnatan Torres | Stone <etorres@stone.com.br>
2018-11-17 11:39:09 -0200

    Start development of application using Clean DDD Architecture

---
+++ src/CMS.ApplicationCore/Entity/Client.cs
@@ -0,0 +1,9 @@
+namespace CMS.ApplicationCore.Entity
+{
+    public class Client
+    {
+        public int ClientId {get; set; }
+        public string Name {get; set; }
+        public string CPF {get; set; }
+    }
+}
